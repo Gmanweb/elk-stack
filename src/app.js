c588598c57ecba96ff714b23a679756cdc692a74
Ryan Neufeld <ryan@rkn.io>
2015-06-30 12:33:21 -0500

    Disable auth on frontend to enable earlier experimentation.
    This was motivated by a bug in the original file's google-oauth.js
    implementation. On all of Firefox, Safari and Chrome, I was unable to
    successfully authenticate. It would seem that when a successful google
    authentication callback returned, the subsequent request's Cookie header
    would not be set to the new authenticated cookie set in the callback.
    
    This causes an indefinite redirect, preventing access to the Kibana
    dashboard.

--- src/app.js
+++ src/app.js
@@ -3,16 +3,18 @@ var http = require('http')
 var fs = require('fs')
 var config = require('./config')
 // var github = require('./lib/github-oauth')
-var github = require('./lib/google-oauth')
-var sessions = require("client-sessions")
+// var github = require('./lib/google-oauth')
+// var sessions = require("client-sessions")
 
 var app = express()
 
 console.log('Logcabin starting...')
 
-app.use(sessions({ cookieName: 'session', secret: config.cookie_secret }))
+// Disable authentication for frontend to enable quicker experimentation.
 
-github.setupOAuth(express, app, config)
+// app.use(sessions({ cookieName: 'session', secret: config.cookie_secret }))
+
+// github.setupOAuth(express, app, config)
 
 proxyES()
 proxyKibana4()
